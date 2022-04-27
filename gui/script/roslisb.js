48ce33bd5c1e4a29ccc195212f7046ac4950b66f
leeminq1 <70461938+leeminq1@users.noreply.github.com>
2022-04-28 08:55:55 +0900

    Add files via upload

--- gui/script/roslisb.js
+++ gui/script/roslisb.js
@@ -1,6 +1,6 @@
 var ros = new ROSLIB.Ros({
-    url : 'ws://localhost:9090'
-    // url : 'ws://192.168.202.26:9090'
+    // url : 'ws://localhost:9090'
+    url : 'ws://192.168.189.35:9090'
   });
 
   ros.on('connection', function() {
@@ -78,8 +78,8 @@ function mapLoad() {
   // Create the main viewer.
   var viewer = new ROS2D.Viewer({
     divID : 'map',
-    width : 600,
-    height : 500
+    width : 1000,
+    height : 1000
   });
 
   // Setup the map client.
@@ -101,8 +101,9 @@ function mapLoad() {
         // Make robot pose subscriber
   const poseTopic = new ROSLIB.Topic({
     ros : ros,
-    name : '/turtle1/pose',
-    messageType : 'turtlesim/Pose'
+    name : '/tf',
+    messageType:'tf2_msgs/TFMessage'
+    // messageType : 'turtlesim/Pose'
     // messageType : 'geometry_msgs/Pose'
   });
 
@@ -110,24 +111,34 @@ function mapLoad() {
 const createFunc = function (handlerToCall, discriminator, robotMarker) {
   return discriminator.subscribe(function(pose){
 
+      // CrtoGrapher slam case(tf2_msgs/TFMessage)
+      let odomPose = pose.transforms[0].transform.translation
+      let baseLinkPose=pose.transforms[1].transform.translation
+      
+      // console.log(`odom_x : ${odomPose.x} , odom_y : ${odomPose.y}`)
+      // console.log(`baseLinkPose_x : ${baseLinkPose.x} , baseLinkPose_y : ${baseLinkPose.y}`)
 
-      //  일반적인 경우 gemometry_msgs/Pose 내에 가져올때
-        // robotMarker.x = pose.pose.pose.position.x;
-        // robotMarker.y = -pose.pose.pose.position.y;
-        // var quaZ = pose.pose.pose.orientation.z;
+
+      //  When using Nav,  gemometry_msgs/Pose .orientation. {x,y,z,w} (Quarternion)  
+      //  When using SLAM  tf2_msgs/TFMessage .transform . rotation  {x,y,z,w} (quarternion)
+      let quaZ=pose.transforms[1].transform.rotation.z
+
+      // pose using odom
+      robotMarker.x = baseLinkPose.x;
+      robotMarker.y = -baseLinkPose.y;
 
       // turtlesim
-      robotMarker.x = pose.x;
-      robotMarker.y = -pose.y;
-      const quaZ=pose.theta
+      // robotMarker.x = pose.x;
+      // robotMarker.y = -pose.y;
 
-        // var degreeZ = 0;
-        // if( quaZ >= 0 ) {
-        //     degreeZ = quaZ / 1 * 180
-        // } else {
-        //     degreeZ = (-quaZ) / 1 * 180 + 180
-        // };
-        robotMarker.rotation = pose.theta;
+
+        let degreeZ = 0;
+        if( quaZ >= 0 ) {
+            degreeZ = quaZ / 1 * 180
+        } else {
+            degreeZ = (-quaZ) / 1 * 180 + 180
+        };
+        robotMarker.rotation = degreeZ;
 
         // rootObject를 통해서 robotMaker에 값을 넣어줌
         gridClient.rootObject.addChild(robotMarker);
